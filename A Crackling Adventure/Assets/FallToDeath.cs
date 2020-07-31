dd9537e870d2ee609f9e5e29ad806827840f0f43
Mash J Prime <mashjprime@gmail.com>
2020-07-31 20:05:37 +0800

    Added the FallToDeath obstacle
    Setup and ready to initiate player death sequence

---
+++ A Crackling Adventure/Assets/FallToDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FallToDeath : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Game Over");
+            GameObject.Find("TestText").GetComponent<Text>().text = "Game Over";
+        }
+    }
+}
